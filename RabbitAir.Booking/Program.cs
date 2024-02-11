4aa66f2c87fdf782ac8cfb56b7da282c17a2afc4
Patryk <patryk.makulski@interia.pl>
2024-02-11 19:55:14 +0100

    refactor: add RabbitMq reusable to consumer

--- RabbitAir.Booking/Program.cs
+++ RabbitAir.Booking/Program.cs
@@ -1,31 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Text;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+using RabbitAir.Booking;
 
 Console.WriteLine("Welcome to Booking service!");
 
-var factory = new ConnectionFactory()
+var broker = new RabbitMq("bookings");
+
+broker.MessageReached += MessageReached;
+
+static void MessageReached(object? sender, string e)
 {
-    HostName = "localhost",
-    UserName = "user",
-    Password = "password",
-    VirtualHost = "/"
-};
-
-
-   var conn = factory.CreateConnection();
-   using var channel = conn.CreateModel();
-   channel.QueueDeclare("bookings", durable:true, exclusive: false);
-
-   var consumer = new EventingBasicConsumer(channel);
-consumer.Received += (model, eventArgs)=>
-{
-    var body = eventArgs.Body.ToArray();
-    var message = Encoding.UTF8.GetString(body);
-    Console.WriteLine($"New booking has been arrived: {message}");
-};
-
-channel.BasicConsume("bookings", true, consumer);
+    Console.WriteLine($"New booking has been arrived: {e}");
+}
 
 Console.ReadKey();
+
+broker.Dispose();
---
+++ RabbitAir.Booking/RabbitMq.cs
@@ -0,0 +1,52 @@
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitAir.Booking;
+
+public class RabbitMq 
+{
+    public event EventHandler<string>? MessageReached;
+    private readonly ConnectionFactory factory = new ConnectionFactory()
+    {
+        HostName = "localhost",
+        UserName = "user",
+        Password = "password",
+        VirtualHost = "/"
+    };
+    private readonly IModel channel;
+    private readonly IConnection conn;
+    private readonly string consumerTag;
+    public RabbitMq(string queueName)
+    {
+        conn = factory.CreateConnection();
+        channel = conn.CreateModel();
+        channel.QueueDeclare(queueName, durable:true, exclusive: false);
+        channel.BasicQos(0, 1, false);
+
+        EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (sender, eventArgs)=>
+        {
+            var body = eventArgs.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            OnMessageReached(this, message);
+
+            channel.BasicAck(eventArgs.DeliveryTag, false);
+        };
+        consumerTag = channel.BasicConsume(queueName, false, consumer);
+    }
+ 
+    protected virtual void OnMessageReached(object source, string message)
+    {
+        EventHandler<string>? handler = MessageReached;
+        if (handler != null)
+            handler(this, message);
+    }
+
+    public void Dispose()
+    {
+        channel.BasicCancel(consumerTag);
+        channel.Close();
+        conn.Close();
+    }
+}
